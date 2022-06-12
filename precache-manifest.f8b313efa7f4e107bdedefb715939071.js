564d7580da5fc86ebf740c303c986e577beb0a18
Deyvis Mariños Castañeda <deyvis.marinos@pragma.com.co>
2022-06-12 02:14:30 -0500

    Updates

---
+++ precache-manifest.f8b313efa7f4e107bdedefb715939071.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371fb34dd950f7d53bb524edceaec29a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d5d6f2da79f07720149e",
+    "url": "./static/css/main.900df4bb.chunk.css"
+  },
+  {
+    "revision": "75db613059a94dde7851",
+    "url": "./static/js/2.a601f6be.chunk.js"
+  },
+  {
+    "revision": "ce2886c9d9d5129f5fe39c8d77b88335",
+    "url": "./static/js/2.a601f6be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5d6f2da79f07720149e",
+    "url": "./static/js/main.ffabca83.chunk.js"
+  },
+  {
+    "revision": "11ddb26551b7d26c0b624912c9ffb006",
+    "url": "./static/js/main.ffabca83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3999cebee7507e84e4c4",
+    "url": "./static/js/runtime-main.506da64f.js"
+  },
+  {
+    "revision": "1f9366a213eb04effabb00d2904e3464",
+    "url": "./static/media/Spinning.1f9366a2.gif"
+  }
+]);
