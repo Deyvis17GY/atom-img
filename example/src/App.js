849b7e80d19bec70da2ca21157df33262d38eed8
Deyvis Mariños Castañeda <deyvis.marinos@pragma.com.co>
2022-06-12 02:29:25 -0500

    feat: fisrt example

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-import { ExampleComponent } from '@deyvis/atom-img'
+import { Picture } from '@deyvis/atom-img'
 import '@deyvis/atom-img/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <main style={{ width: '200px', height: '200px' }}>
+      <Picture classPicture="picture" alt="image" url="https://images5.alphacoders.com/958/thumb-1920-958580.jpg" />
+    </main>
+  )
 }
 
 export default App
